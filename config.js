0b2ffefac5927b6d018c9f2f4385105c0318f115
Azureowl <maiaroche16@gmail.com>
2018-10-27 12:54:15 -0700

    added local host for client origin

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ require('dotenv').config();
 exports.DB_URL = process.env.DB_URL;
 exports.YT_key = process.env.YT_key;
 exports.PORT = process.env.PORT || 8080;
-exports.CLIENT_ORIGIN = 'https://popcorn-capstone.herokuapp.com';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+// 'https://popcorn-capstone.herokuapp.com';
