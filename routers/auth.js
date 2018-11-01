7b7c05dc8765cb4e2afeed17aba91645dc63071f
Azureowl <maiaroche16@gmail.com>
2018-10-31 19:00:28 -0700

    returned body not including username--fixing

--- routers/auth.js
+++ routers/auth.js
@@ -1,36 +1,35 @@
-const {User} = require("../models/users");
+const { User } = require("../models/users");
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
 
-
-router.post('/signup', (req, res) => {
-  console.log(req.body, 'auth.js, line 11');
+router.post("/signup", (req, res) => {
+  console.log(req.body, "auth.js, line 11");
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
 
   User.create({
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
   })
-  .then(user => {
+    .then(user => {
       console.log("returned user is: ", user);
-      return res.json({user: user.username});
-  })
-  .catch(err => {
-    if (err.code === 11000) {
-        return res.status(400).json({error: "User already exists."});
-    }
-  });
+      return res.json({ user: user.username });
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        return res.status(400).json({ error: "User already exists." });
+      }
+    });
 });
 
-router.post("/login", function (req, res, next) {
-  console.log(req.body, 'line 32 auth.js');
-  passport.authenticate('login', {session: false}, (err, user, info) => {
+router.post("/login", function(req, res, next) {
+  console.log(req.body, "line 32 auth.js");
+  passport.authenticate("login", { session: false }, (err, user, info) => {
     if (err || !user) {
       return res.status(400).json({
         message: info.message,
@@ -39,9 +38,14 @@ router.post("/login", function (req, res, next) {
     }
     req.login(user, { session: false }, err => {
       if (err) {
-        res.status(400).json({err});
+        res.status(400).json({ err });
       }
-      const body = { _id : user._id, email : user.email, username : user.username};
+      const body = {
+        _id: user._id,
+        email: user.email,
+        username: user.username,
+        test: 'HOWDY!'
+      };
       const token = jwt.sign(body, JWT_SECRET);
       return res.json({ token });
     });
