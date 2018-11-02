ca30121970abbef7e1ab325883752e5232a1ca0a
Azureowl <maiaroche16@gmail.com>
2018-11-02 13:06:29 -0700

    Integrated test socket.io chat app

--- server.js
+++ server.js
@@ -1,94 +1,118 @@
-const {
-  PORT,
-  DB_URL,
-  TEST_DB_URL,
-  YT_key,
-  CLIENT_ORIGIN
-} = require("./config");
-const express = require("express");
-const cors = require("cors");
-const socket_io = require("socket.io");
+const { PORT, DB_URL, TEST_DB_URL, YT_key, CLIENT_ORIGIN } = require('./config');
+const express = require('express');
+const cors = require('cors');
+var http = require('http');
+var socket_io = require('socket.io');
 const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+var serverSocket = http.Server(app);
+var io = socket_io(serverSocket);
+
+
 
 app.use(cors());
-app.options("*", cors());
+app.options('*', cors());
 
-const morgan = require("morgan");
-const mongoose = require("mongoose");
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const passport = require("passport");
-require("./passport");
+const passport = require('passport');
+require('./passport');
 
-const videoRouter = require("./routers/videoRouter");
-const auth = require("./routers/auth");
-const userRouter = require("./routers/userRouter");
+const videoRouter = require('./routers/videoRouter');
+const auth = require('./routers/auth');
+const userRouter = require('./routers/userRouter');
 
 app.use(express.json());
-app.use(morgan("common"));
-app.use(
-  "/videos",
-  passport.authenticate("jwt", { session: false }),
-  videoRouter
-);
-app.use("/auth", auth);
-app.use("/user", passport.authenticate("jwt", { session: false }), userRouter);
+app.use(morgan('common'));
+app.use('/videos', passport.authenticate('jwt', {session: false}), videoRouter);
+app.use('/auth', auth);
+app.use('/user', passport.authenticate('jwt', {session: false}), userRouter);
 
 let server;
 
 function runServer(databaseUrl, port = PORT) {
-  console.log(port);
-  return new Promise((resolve, reject) => {
-    mongoose
-      .connect(
-        databaseUrl,
-        {
-          useNewUrlParser: true
-        },
-        err => {
-          if (err) {
-            return reject("Something went wrong");
-          }
-          server = http
-            .listen(port, () => {
-              console.log(`Your app is listening on port ${port}!`);
-              resolve();
-            })
-            .on("error", err => {
-              mongoose.disconnect();
-              reject("Unable to connect to port. Disconnecting Mongoose.");
+    console.log(port);
+    return new Promise((resolve, reject) => {
+        mongoose
+            .connect(
+                databaseUrl, {
+                    useNewUrlParser: true
+                },
+                err => {
+                    if (err) {
+                        return reject("Something went wrong");
+                    }
+                    server = serverSocket.listen(port, () => {
+                            console.log(`Your app is listening on port ${port}!`);
+                            io.on('connect', function(socket) {
+                                socket.on('chat message', function(msg) {
+                                  io.emit('chat message', msg);
+                                });
+                            });
+                            resolve();
+                        })
+                        .on("error", err => {
+                            mongoose.disconnect();
+                            reject("Unable to connect to port. Disconnecting Mongoose.");
+                        });
+                }
+            )
+            .catch(err => {
+                console.log("Unable to connect to Mongoose.");
             });
-        }
-      )
-      .catch(err => {
-        console.log("Unable to connect to Mongoose.");
-      });
-  });
+    });
 }
 
 function closeServer() {
-  return mongoose.disconnect().then(() => {
-    return new Promise((resolve, reject) => {
-      console.log("Closing server");
-      server.close(err => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
     });
-  });
 }
 
 if (require.main === module) {
-  console.log(DB_URL, PORT);
-  runServer(DB_URL, PORT).catch(err => {
-    console.log(`In ${require.main.filename}`);
-    console.error(err);
-  });
+    console.log(DB_URL, PORT);
+    runServer(DB_URL, PORT).catch(err => {
+        console.log(`In ${require.main.filename}`);
+        console.error(err);
+    });
 }
+
+var usersArray = [];
+
+function sendMessage(msg) {
+    var user = {
+        msg: msg
+    }
+
+  return user.msg;
+}
+
+app.post('/user/chat', (req, res) => {
+    io.on('connect', function (socket) {
+        console.log('connected!');
+        socket.on('chat message', function(msg) {
+            console.log(`connected here! please! ${msg}`);
+            io.emit('chat message', msg);
+        });
+        // closing page disconnects
+        socket.on('disconnect', function(){
+            console.log('user disconnected');
+        });
+    });
+
+    res.json({msg: req.params.text});
+});
+
 
 exports.app = app;
 exports.runServer = runServer;
 exports.closeServer = closeServer;
+
