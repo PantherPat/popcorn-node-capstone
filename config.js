0b13ec86500965133eb318c29f234f58f8ec58e9
Azureowl <maiaroche16@gmail.com>
2018-11-07 12:02:24 -0800

    Updated tests

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 exports.DB_URL = process.env.DB_URL;
+exports.TEST_DB_URL = process.env.TEST_DB_URL;
 exports.YT_key = process.env.YT_key;
 exports.PORT = process.env.PORT || 8080;
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
--- test/test-server.js
+++ test/test-server.js
@@ -1,84 +1,160 @@
-// const { YT_key } = require('../config');
-// const { app, runServer, closeServer } = require("../server");
+const { app, runServer, closeServer } = require("../server");
+const { YT_key, TEST_DB_URL } = require("../config");
+const { User } = require("../models/users");
+const { Videos } = require("../models/videos");
+var ObjectId = require("mongodb").ObjectID;
 
-// // add testDB later
-// const chai = require("chai");
-// const chaiHTTP = require("chai-http");
-// const expect = chai.expect;
+const chai = require("chai");
+const chaiHTTP = require("chai-http");
+const mongoose = require("mongoose");
+const expect = chai.expect;
 
-// chai.use(chaiHTTP);
+chai.use(chaiHTTP);
 
-// describe("API", function() {
-// // GET
-//  it("should send back search results", function() {
-//     const term = "eighties";
-//     return chai
-//     .request(app)
-//     .get(`/videos`)
-//     .query({ part: 'snippet', q: term, key: YT_key, maxResults: 2 })
-//     .then(function(res) {
-//         expect(res).to.have.status(200);
-//         expect(res).to.be.json;
-//       });
-//   });
+function genUser() {
+  const user = {
+    username: "boston",
+    email: "boston@gmail.com",
+    password: "happy"
+  };
 
-//   it("should get all videos and status code 200", function() {
-//     return chai
-//     .request(app)
-//     .get(`/videos`)
-//     .then(function(res) {
-//         expect(res).to.have.status(200);
-//         expect(res).to.be.json;
-//       });
-//   });
+  return User.create(user).then(user => {
+    return user;
+  });
+}
 
-// //POST
-// //   it("should post a video and status code 201", function() {
-// //     const videoID = '1234';
-// //     const title = 'Test title';
-// //     const thumbnail = 'test.png';
-// //     const user = 'Test Person1';
+function genJWTToken() {
+  let token;
+  const creds = {
+    email: "boston@gmail.com",
+    password: "happy"
+  };
 
-// //     return chai
-// //     .request(app)
-// //     .post(`/videos`)
-// //     .send({
-// //         videoID, title, thumbnail, user
-// //     })
-// //     .then(function(res) {
-// //         expect(res).to.have.status(201);
-// //         expect(res).to.be.json;
-// //         expect(res.body).to.be.an('object');
-// //         expect(res.body).to.include.all.keys('videoID', 'title', 'thumbnail', 'user');
-// //       });
-// //   });
+  return chai
+    .request(app)
+    .post("/auth/login")
+    .send(creds)
+    .then(function(res) {
+      expect(res).to.have.status(200);
+      token = res.body.token;
+      return token;
+    });
+}
 
-//   //PUT
-//   it("should get all videos and status code 201", function() {
-//     const time = '120';
-//     return chai
-//     .request(app)
-//     .put(`/videos/34567/${time}`)
-//     .send({
-//         time
-//     })
-//     .then(function(res) {
-//         expect(res).to.have.status(201);
-//         expect(res).to.be.json;
-//         expect(res.body).to.be.an('object');
-//         expect(res.body).to.include.all.keys('time');
-//       });
-//   });
+function tearDownDb() {
+  console.warn("Deleting database");
+  return mongoose.connection.dropDatabase();
+}
 
-// //DELETE
-// it("should delete video and have status code 204", function() {
-//     const id = '34567';
-//     return chai
-//     .request(app)
-//     .delete(`/videos/34567`)
-//     .then(function(res) {
-//         expect(res).to.have.status(204);
-//       });
-//   });
+describe("Popcorn Test", function() {
+  before(function() {
+    return runServer(TEST_DB_URL);
+  });
 
-// });
+  beforeEach(function() {
+    return genUser();
+  });
+
+  afterEach(function() {
+    return tearDownDb();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  // ******* TESTING GET ******* //
+  describe("GET videos endpoint", function() {
+    it("Should get search results", function() {
+      return genJWTToken().then(token => {
+        return User.find({ username: "boston" }).then(user => {
+          return chai
+            .request(app)
+            .get("/videos/search/eighties")
+            .set("Authorization", `Bearer ${token}`)
+            .query({
+              part: "snippet",
+              q: "eighties",
+              key: YT_key,
+              maxResults: 2
+            })
+            .then(function(res) {
+              expect(res).to.have.status(200);
+              expect(res).to.be.json;
+              expect(res.body).to.be.an("object");
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        });
+      });
+    });
+  });
+
+  // ******* TESTING POST ******* //
+  describe("POST videos endpoint", function() {
+    it("Should post to watchlist", function() {
+      return genJWTToken().then(token => {
+        return User.find({ username: "boston" }).then(user => {
+          const videoObj = {
+            id: "M4Ufs7-FpvU",
+            title: "Best Movie Soundtracks (Top 10 HD)",
+            thumbnail: "https://i.ytimg.com/vi/M4Ufs7-FpvU/mqdefault.jpg"
+          };
+
+          const userVideo = { video: videoObj, id: user[0]._id };
+
+          return chai
+            .request(app)
+            .post("/videos")
+            .send(userVideo)
+            .set("Authorization", `Bearer ${token}`)
+            .then(function(res) {
+              expect(res).to.have.status(200);
+              expect(res).to.be.json;
+              expect(res.body).to.be.an("object");
+            });
+        });
+      });
+    });
+  });
+
+  // ******* TESTING DELETE ******* //
+  describe("DELETE video endpoint", function() {
+    it("should delete video and have status code 204", function() {
+      return genJWTToken().then(token => {
+        return User.find({ username: "boston" }).then(user => {
+          const videoObj = {
+            id: "M4Ufs7-FpvU",
+            title: "Best Movie Soundtracks (Top 10 HD)",
+            thumbnail: "https://i.ytimg.com/vi/M4Ufs7-FpvU/mqdefault.jpg"
+          };
+
+          const userVideo = { video: videoObj, id: user[0]._id };
+
+          return chai
+            .request(app)
+            .post("/videos")
+            .send(userVideo)
+            .set("Authorization", `Bearer ${token}`)
+            .then(function(res) {
+              expect(res).to.have.status(200);
+              expect(res).to.be.json;
+              expect(res.body).to.be.an("object");
+              let videoID = res.body._id;
+              return chai
+                .request(app)
+                .delete(`/videos/${videoID}`)
+                .set("Authorization", `Bearer ${token}`)
+                .then(function(res) {
+                  expect(res).to.have.status(204);
+                  Videos.find({ _id: ObjectId(videoID) }).then(video => {
+                    expect(video).to.have.lengthOf(0);
+                  });
+                });
+            });
+        });
+      });
+    });
+  });
+});
